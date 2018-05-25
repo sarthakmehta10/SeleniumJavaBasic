729ad923e0d235a2b55e6f19d5e963603caf1d26
Sarthak Mehta <sarthak_mehta@epam.com>
2018-05-25 12:53:46 +0530

    Update GenericFunctions.java

--- src/test/java/com/genericfunctions/GenericFunctions.java
+++ src/test/java/com/genericfunctions/GenericFunctions.java
@@ -7,15 +7,14 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GenericFunctions {
-	private static WebDriver driver;
 
-	public void waitForElementToBeVisible(WebElement element) {
-		WebDriverWait wait = new WebDriverWait(driver, 10);
+	public void waitForElementToBeVisible(WebDriver driver, WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, 50);
 		wait.until(ExpectedConditions.visibilityOf(element));
 	}
 
-	public void waitForElementToBeClickable(WebElement element) {
-		WebDriverWait wait = new WebDriverWait(driver, 10);
+	public void waitForElementToBeClickable(WebDriver driver, WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, 50);
 		wait.until(ExpectedConditions.elementToBeClickable(element));
 	}
 
